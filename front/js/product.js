3b51adc681beff91f6a16154c11349231fabdd4c
Sacha Malécot <sacha.malecot@gmail.com>
2022-12-13 19:54:46 +0100

    addtocart correction

--- front/js/product.js
+++ front/js/product.js
@@ -2,8 +2,8 @@
 // Récupération de l'ID depuis l'URL
 //------------------------------------------------------------------------ 
 
-let params = new URLSearchParams(document.location.search)
-let id = params.get("_id")
+const params = new URLSearchParams(document.location.search);
+const id = params.get("_id");
 
 //------------------------------------------------------------------------
 // Récupération du produit souhaité via l'api
@@ -19,21 +19,22 @@ fetch(`http://localhost:3000/api/products/${id}`)
     });
 
 //------------------------------------------------------------------------
-//  Création de la classe de l'objet à ajouter au panier
+//  Création du panier
 //------------------------------------------------------------------------ 
 
-class Product {
-    constructor(_id, color) {
-        this._id = _id;
-        this.color = color;
-    }
-};
+let cartStorage = [];
+
+//------------------------------------------------------------------------
+//  Création de l'objet à ajouter au panier
+//------------------------------------------------------------------------ 
+
+let itemToCart = { id: id, color: "", quantity: 1};
 
 //------------------------------------------------------------------------
 //  Fonction d'affichage du produit
 //------------------------------------------------------------------------ 
 
-const product = (productItem) => {
+const product = (productItem)=> {
     const {imageUrl, altTxt, name, price, description, colors} = productItem;
     let productImage = document.querySelector(".item__img");
     let productTitle = document.querySelector("#title");
@@ -52,73 +53,68 @@ const product = (productItem) => {
 }
 
 //------------------------------------------------------------------------
-//  Choix de la couleur et création de l'objet "Product"
+//  Choix de la couleur
 //------------------------------------------------------------------------ 
 
-let colorChoice = document.querySelector("#colors");
 let itemToCartColor;
-let itemToCart;
 
-colorChoice.addEventListener("input", (col) => {
+document.querySelector("#colors").addEventListener("input", (col) => {
     itemToCartColor = col.target.value;
+    itemToCart.color = itemToCartColor;
     document.querySelector("#addToCart").style.color = "white";
     document.querySelector("#addToCart").textContent = "Ajouter au panier";
-    itemToCart = new Product (id, itemToCartColor)
 });
 
 //------------------------------------------------------------------------
 //  Choix de la quantité
 //------------------------------------------------------------------------ 
 
-let quantityChoice = document.querySelector("#quantity");
 let itemToCartQuantity;
 
-quantityChoice.addEventListener("input", (qty) => {
+document.querySelector("#quantity").addEventListener("input", (qty) => {
     itemToCartQuantity = parseInt(qty.target.value);
+    itemToCart.quantity = itemToCartQuantity;
     document.querySelector("#addToCart").style.color = "white";
     document.querySelector("#addToCart").textContent = "Ajouter au panier";
 });
 
-
 //------------------------------------------------------------------------
-//  Création du panier
+//  Vérification de la quantité et de la couleur lors de l'ajout au panier
 //------------------------------------------------------------------------ 
 
-let cartStorage = new Map();
-
-//------------------------------------------------------------------------
-//  Fonction d'ajout au panier
-//------------------------------------------------------------------------ 
-
-const cart = () => {
-    let exist = cartStorage.has(itemToCart);
-    if (!exist) {
-        cartStorage.set(itemToCart, itemToCartQuantity);
-        console.log(cartStorage)
-    } else {
-        cartStorage.set(itemToCart, itemToCartQuantity + cartStorage.get(itemToCart));
-        console.log(cartStorage)
-    }
-}
-
-//------------------------------------------------------------------------
-//  Ajout au panier avec vérification de la quantité et de la couleur 
-//------------------------------------------------------------------------ 
-
-let addItemToCart = document.querySelector("#addToCart");
-addItemToCart.addEventListener("click", () => {
-    const color = itemToCart.color;
-    const quantity = itemToCartQuantity;
-    if ( color && quantity && (quantity >= 1 && quantity <= 100)){
+document.querySelector("#addToCart").addEventListener("click", () => {
+    console.log("produit a mettre au panier" , itemToCart);
+    console.log("Panier avant l'ajout" , cartStorage);
+    const { quantity, color } = itemToCart;
+    if (color && quantity && (quantity >= 1 && quantity <= 100)){
         document.querySelector("#addToCart").style.color = "rgb(0, 205, 0)";
         document.querySelector("#addToCart").textContent = "Ajouté !";
         cart();
-        
     } else {
         alert("Veuillez choisir une couleur et/ou une quantité entre 1 et 100");
     }
 });
 
 //------------------------------------------------------------------------
+//  Fonction d'ajout au panier
+//------------------------------------------------------------------------ 
+
+const cart = () => {
+    const search = (element) => (element.id === itemToCart.id && element.color === itemToCart.color);
+    const index = cartStorage.findIndex(search);
+    console.log(index > -1? `L'index du produit est ${index}` : "Le produit n'existe pas encore dans le panier")
+    if (index > -1) {
+        console.info("On update les quantités", cartStorage[index].quantity, " + ", itemToCart.quantity)
+        cartStorage[index].quantity = cartStorage[index].quantity + itemToCart.quantity;
+    } else {
+        cartStorage = [...cartStorage, itemToCart]
+    }
+    console.log("Panier après l'ajout" , cartStorage);
+    itemToCart = { id: id, color: itemToCartColor, quantity: itemToCartQuantity};
+
+}
+
+//------------------------------------------------------------------------
 //  Ajout du produit du panier dans le local storage 
 //------------------------------------------------------------------------
+
