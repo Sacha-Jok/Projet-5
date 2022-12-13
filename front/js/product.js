ca1e3f7e07a91040b03e5f7507e3ad3946317571
Sacha Malécot <sacha.malecot@gmail.com>
2022-12-13 01:44:53 +0100

    map creation

--- front/js/product.js
+++ front/js/product.js
@@ -19,17 +19,21 @@ fetch(`http://localhost:3000/api/products/${id}`)
     });
 
 //------------------------------------------------------------------------
-//  Création de l'objet à ajouter au panier
+//  Création de la classe de l'objet à ajouter au panier
 //------------------------------------------------------------------------ 
 
-let itemToCart = {};
-itemToCart._id = id;
+class Product {
+    constructor(_id, color) {
+        this._id = _id;
+        this.color = color;
+    }
+};
 
 //------------------------------------------------------------------------
 //  Fonction d'affichage du produit
 //------------------------------------------------------------------------ 
 
-function product(productItem){
+const product = (productItem) => {
     const {imageUrl, altTxt, name, price, description, colors} = productItem;
     let productImage = document.querySelector(".item__img");
     let productTitle = document.querySelector("#title");
@@ -48,6 +52,21 @@ function product(productItem){
 }
 
 //------------------------------------------------------------------------
+//  Choix de la couleur et création de l'objet "Product"
+//------------------------------------------------------------------------ 
+
+let colorChoice = document.querySelector("#colors");
+let itemToCartColor;
+let itemToCart;
+
+colorChoice.addEventListener("input", (col) => {
+    itemToCartColor = col.target.value;
+    document.querySelector("#addToCart").style.color = "white";
+    document.querySelector("#addToCart").textContent = "Ajouter au panier";
+    itemToCart = new Product (id, itemToCartColor)
+});
+
+//------------------------------------------------------------------------
 //  Choix de la quantité
 //------------------------------------------------------------------------ 
 
@@ -55,39 +74,51 @@ let quantityChoice = document.querySelector("#quantity");
 let itemToCartQuantity;
 
 quantityChoice.addEventListener("input", (qty) => {
-    itemToCartQuantity = qty.target.value;
-    itemToCart.quantity = itemToCartQuantity;
+    itemToCartQuantity = parseInt(qty.target.value);
     document.querySelector("#addToCart").style.color = "white";
     document.querySelector("#addToCart").textContent = "Ajouter au panier";
 });
 
+
 //------------------------------------------------------------------------
-//  Choix de la couleur
+//  Création du panier
 //------------------------------------------------------------------------ 
 
-let colorChoice = document.querySelector("#colors");
-let itemToCartColor;
-
-colorChoice.addEventListener("input", (col) => {
-    itemToCartColor = col.target.value;
-    itemToCart.color = itemToCartColor;
-    document.querySelector("#addToCart").style.color = "white";
-    document.querySelector("#addToCart").textContent = "Ajouter au panier";
-});
+let cartStorage = new Map();
 
 //------------------------------------------------------------------------
-//  Vérification de la quantité et de la couleur lors de l'ajout au panier
+//  Fonction d'ajout au panier
+//------------------------------------------------------------------------ 
+
+const cart = () => {
+    let exist = cartStorage.has(itemToCart);
+    if (!exist) {
+        cartStorage.set(itemToCart, itemToCartQuantity);
+        console.log(cartStorage)
+    } else {
+        cartStorage.set(itemToCart, itemToCartQuantity + cartStorage.get(itemToCart));
+        console.log(cartStorage)
+    }
+}
+
+//------------------------------------------------------------------------
+//  Ajout au panier avec vérification de la quantité et de la couleur 
 //------------------------------------------------------------------------ 
 
 let addItemToCart = document.querySelector("#addToCart");
 addItemToCart.addEventListener("click", () => {
-    const { quantity, color } = itemToCart;
-    if (color && quantity && (quantity >= 1 && quantity <= 100)){
+    const color = itemToCart.color;
+    const quantity = itemToCartQuantity;
+    if ( color && quantity && (quantity >= 1 && quantity <= 100)){
         document.querySelector("#addToCart").style.color = "rgb(0, 205, 0)";
         document.querySelector("#addToCart").textContent = "Ajouté !";
         cart();
+        
     } else {
         alert("Veuillez choisir une couleur et/ou une quantité entre 1 et 100");
     }
 });
 
+//------------------------------------------------------------------------
+//  Ajout du produit du panier dans le local storage 
+//------------------------------------------------------------------------
