27ba1491f756dc4bd30e1a0fa2d6ac2da48f5aa1
Sacha Malécot <sacha.malecot@gmail.com>
2022-12-15 21:03:32 +0100

    add cart remove button

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,7 +2,7 @@
 // Récupération du panier depuis le local storage
 //------------------------------------------------------------------------ 
 
-const cartProduct = JSON.parse(localStorage.getItem("Cart"));
+const cartStorage = JSON.parse(localStorage.getItem("Cart"));
 
 //----------------------------------------------------------------------
 // Affichage des articles de l'api sur la homepage
@@ -10,21 +10,31 @@ const cartProduct = JSON.parse(localStorage.getItem("Cart"));
 
 const cart = () => {
   let cartSum = 0
-  for (let i = 0; i < cartProduct.length ;i++ ) {
-    fetch(`http://localhost:3000/api/products/${cartProduct[i].id}`)
+  let cartQuantity = 0
+  for (let i = 0; i < cartStorage.length ;i++ ) {
+    fetch(`http://localhost:3000/api/products/${cartStorage[i].id}`)
     .then ((res)=> res.json())
     .then ((recoveredApiInfo)=>{
-        cartSum += recoveredApiInfo.price
-        fillCart(recoveredApiInfo, cartProduct[i], cartSum)
+        cartSum += (recoveredApiInfo.price * cartStorage[i].quantity)
+        cartQuantity += cartStorage[i].quantity
+        fillCart(recoveredApiInfo, cartStorage[i], cartSum, cartQuantity)
     })
-  }   
+  }  
+}
+
+//------------------------------------------------------------------------
+// Modification du titre si le panier est vide
+//------------------------------------------------------------------------ 
+
+if (cartStorage.length == 0) {
+  document.querySelector("#cartAndFormContainer").innerHTML = `<h1>Votre panier est vide</h1>`
 }
 
 //----------------------------------------------------------------------
 // Génération du bloc de chaque article du panier
 //----------------------------------------------------------------------
 
-const fillCart = (productInfo, product, cartSum) => {
+const fillCart = (productInfo, product, cartSum, cartQuantity) => {
   document.querySelector("#cart__items").innerHTML +=
     `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
     <div class="cart__item__img">
@@ -34,7 +44,7 @@ const fillCart = (productInfo, product, cartSum) => {
       <div class="cart__item__content__description">
         <h2>${productInfo.name}</h2>
         <p>${product.color}</p>
-        <p>${productInfo.price}</p>
+        <p>${productInfo.price} €</p>
       </div>
       <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
@@ -47,13 +57,32 @@ const fillCart = (productInfo, product, cartSum) => {
       </div>
     </div>
     </article>`
+    document.querySelector("#totalQuantity").innerText = `${cartQuantity}`
     document.querySelector("#totalPrice").innerText = `${cartSum}`
+    deleteItem()
+    changeQuantity()
 }
 
 //----------------------------------------------------------------------
-// Execution de la fonction
+// Execution des fonctions
 //----------------------------------------------------------------------
 
 cart()
 
-  
+//----------------------------------------------------------------------
+// Fonction de suppression d'un article du panier
+//----------------------------------------------------------------------
+
+const deleteItem = () => {
+  document.querySelectorAll(".deleteItem").forEach((deleteOnClick) => {
+    deleteOnClick.addEventListener("click", () => {
+      const idToDelete = deleteOnClick.closest(".cart__item").getAttribute('data-id');
+      const itemToDelete = cartStorage.findIndex((obj) => obj.id === idToDelete);
+      if (itemToDelete !== -1) {
+        cartStorage.splice(itemToDelete, 1)
+      }
+      window.localStorage.setItem("Cart", JSON.stringify(cartStorage));
+      return location.reload();
+    })
+  })
+}
